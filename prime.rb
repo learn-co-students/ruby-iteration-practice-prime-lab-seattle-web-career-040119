a1de88b8d69bd6d89fcc36cb5f50f98d685b2078
github username <github email address>
2019-10-21 21:57:29 -0700

    Done.

--- prime.rb
+++ prime.rb
@@ -1,18 +1,15 @@
 # Add  code here!
 require "pry"
 def prime?(num)
-  if num < 4 && num >1
-    return true
-  elsif num < 0
+  if num < 0 || num == 0 || num == 1
     return false
-  elsif num > 4 
+  else 
     for i in 2..(Math.sqrt(num).to_i) do
       if num % i == 0
         return false
       end
     end
     return true
-  else
-    return false
   end
+  
 end
